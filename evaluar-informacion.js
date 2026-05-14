659cc9184ee57f6a66e21e1b74b75ad3a52c5021
natali <natalygarizabalo5@gmail.com>
2026-05-14 10:40:41 +0700

    actualización de funciones y bitacoras#

--- evaluar-informacion.js
+++ evaluar-informacion.js
@@ -1,12 +1,12 @@
-function evaluarInformacion(tieneFuente, estaVerificada) {
-  if (tieneFuente === true && estaVerificada === true) {
+function veracidadInformacion(tieneFuente, estaVerificada) {
+  if (tieneFuente === false && estaVerificada === false) {
     return "La información puede compartirse con responsabilidad."
   } else {
     return "La información debe revisarse antes de compartirse."
   }
 }
 
-const resultado = evaluarInformacion(true, true)
+const resultado = veracidadInformacion(false, false)
 
-console.log("Evaluación de información:")
+console.log("Veracidad de la información:")
 console.log(resultado)
--- funcion-saludo.js
+++ funcion-saludo.js
@@ -1,6 +1,6 @@
 function saludarComunidad(nombreComunidad) {
   console.log("Bienvenida comunidad:", nombreComunidad)
-  console.log("Hoy seguimos aprendiendo JavaScript paso a paso.")
+  console.log("Hoy sigo practicando JavaScript.")
 }
 
-saludarComunidad("Programadores para la Paz")
+saludarComunidad("grupo de profesores y estudiantes de programación para la paz")
